c9de867a833b54c17aa0159db50ab30195d525b6
須古純平 <jumpeisuko@JumpeiSukos-m2.local>
2022-12-08 21:11:47 +0900

    test

--- src/index.js
+++ src/index.js
@@ -3,12 +3,10 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
-import { AnalyticsWrapper } from './routes/Analitics';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <BrowserRouter>
     <App />
-    <AnalyticsWrapper />
   </BrowserRouter>
 );
