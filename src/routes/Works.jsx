7d677e340fde96d7d992585ea9b55491cb683e11
須古純平 <jumpeisuko@JumpeiSukos-m2.local>
2022-12-16 12:17:18 +0900

    move scroll effect in useEffect

--- src/routes/Works.jsx
+++ src/routes/Works.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import Data from './Data'
 import WorksInner from './WorksInner.jsx'
 import WorksFilter from "./WorksFilter";
@@ -14,6 +14,36 @@ const Works = () => {
         setItem(newItem);
     }
 
+    useEffect(() => {
+        window.addEventListener('scroll', scroll_effect);
+        return () => {
+            window.removeEventListener('scroll', scroll_effect);
+        }
+    }, []);
+
+    function scroll_effect() {
+        const element = document.getElementsByClassName('works__inner');
+        if (!element) return;
+    
+        const scrollY = window.pageYOffset;
+        const windowH = window.innerHeight;
+        var showTiming = 0;
+    
+        for (let i = 0; i < element.length; i++) {
+            let elemClientRect = element[i].getBoundingClientRect();
+            let elemTop = scrollY + elemClientRect.top;
+            let elemBottom = scrollY + elemClientRect.bottom;
+            if (scrollY > elemTop - windowH + showTiming && element[i].classList.contains('is-active')) {
+                element[i].classList.remove('is-active');
+                element[i].children[1].classList.remove('show');
+            };
+            if (elemBottom - windowH / 2 + showTiming > scrollY && scrollY > elemTop - windowH / 2 + showTiming && !element[i].classList.contains('is-active')) {
+                element[i].classList.add('is-active');
+                element[i].children[1].classList.add('show');
+            };
+        };
+    };
+
     return (
         <>
             <section id="works">
@@ -25,30 +55,5 @@ const Works = () => {
         </>
     );
 };
-
-function scroll_effect() {
-    const element = document.getElementsByClassName('works__inner');
-    if (!element) return;
-
-    const scrollY = window.pageYOffset;
-    const windowH = window.innerHeight;
-    var showTiming = 0;
-
-    for (let i = 0; i < element.length; i++) {
-        let elemClientRect = element[i].getBoundingClientRect();
-        let elemTop = scrollY + elemClientRect.top;
-        let elemBottom = scrollY + elemClientRect.bottom;
-        if (scrollY > elemTop - windowH + showTiming && element[i].classList.contains('is-active')) {
-            element[i].classList.remove('is-active');
-            element[i].children[1].classList.remove('show');
-        };
-        if (elemBottom - windowH / 2 + showTiming > scrollY && scrollY > elemTop - windowH / 2 + showTiming && !element[i].classList.contains('is-active')) {
-            element[i].classList.add('is-active');
-            element[i].children[1].classList.add('show');
-        };
-    };
-};
-
-window.addEventListener('scroll', scroll_effect);
 
 export default Works
