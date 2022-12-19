58c5c9a6c3297622935bd055721542506d754e05
須古純平 <jumpeisuko@JumpeiSukos-m2.local>
2022-12-19 16:27:14 +0900

    test

--- src/routes/WorksDetail.jsx
+++ src/routes/WorksDetail.jsx
@@ -4,9 +4,8 @@ import Data from './Data'
 
 const WorksDetail = () => {
   const { paramsId } = useParams();
-  console.log(paramsId);
   const data = Data.find(({ id }) => id === paramsId);
-  console.log(data);
+
   useEffect(()=>{
     if (data.category === "movie") {
       document.getElementById('worksDetail__contentWrap').innerHTML = "<div id='worksDetail__movieWrap'><iframe loading='lazy' width='100%' height='100%' allowfullscreen='1' src=" + `https://www.youtube-nocookie.com/embed/${data.image}?controls=1&autoplay=1&mute=1&loop=1&playlist=${data.image}&rel=0` + " frameborder='0' allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture' allowfullscreen></iframe></div>";
@@ -17,14 +16,14 @@ const WorksDetail = () => {
     }
   },[]);
 
+  console.log(data.sentence.split('\n'))
   return (
     <section className="wrapper" id="worksDetail">
-      <h2 id='worksDetail__category'>{data.category}</h2>
-      <h2 id='worksDetail__title'>{data.title}</h2>
       <div id='worksDetail__contentWrap' className='worksDetail__imageWrap'>
         <img className='worksDetail__image' src={`/${data.image}`} alt="" />
       </div>
-      <p id='worksDetail__sentence'>{data.sentence}</p>
+      <h2 id='worksDetail__title'>[{data.title}]</h2>
+      <p id='worksDetail__sentence'>{data.sentence.split('\n').map(t => (<>{t}<br/></>))}</p>
       <Link to={`/works/`}><p>back to works</p></Link>
     </section>
   )
