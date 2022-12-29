0651d0ec0998c563ec354a0e8132d090f2b0c045
lily2oo <jumpei102113@icloud.com>
2022-12-29 17:28:24 +0900

    add works

--- src/routes/Works.js
+++ src/routes/Works.js
@@ -1,6 +1,6 @@
 const Works = [
     {
-        id :"0",
+        id: "0",
         category: "movie",
         content: "images/image/aquarium.mp4",
         thumb: "images/thumb/worksThumb1.webp",
@@ -18,36 +18,8 @@ const Works = [
         だれもいない岩陰の中に僕の声は木霊した。まぶたを開くと、壁の外を泳ぐ発光性のクラゲがこちらを見つめていた。"
     },
     {
-        id :"1",
-        category: "design",
-        content: "images/image/worksImage2.webp",
-        thumb: "images/thumb/worksThumb2.webp",
-        title: "night in the train.",
-        lang: "en",
-        sentence: "This is my first design piece and is my business card as a programming educator. The motif is a ticket, and the purpose of this work was to communicate what I value and to get people interested in learning more about me."
-    },
-    {
-        id :"2",
-        category: "design",
-        content: "images/image/worksImage3.webp",
-        thumb: "images/thumb/worksThumb3.webp",
-        title: "future.",
-        lang: "en",
-        sentence: "I don't think about my future deeply. "
-    },
-    {
-        id :"3",
-        category: "design",
-        content: "images/image/worksImage4.webp",
-        thumb: "images/thumb/worksThumb4.webp",
-        title: "witch",
-        lang: "en",
-        sentence: "illustration"
-    },
-    {
-        id :"4",
+        id: "1",
         category: "movie",
-        // content: "WKHJRmad4gI",
         content: "images/image/time.mp4",
         thumb: "images/thumb/worksThumb5.webp",
         title: "2001.12.11 - 2022.05.27",
@@ -61,28 +33,29 @@ const Works = [
         But then I fall asleep again."
     },
     {
-        id :"5",
+        id: "2",
         category: "design",
         content: "images/image/worksImage6.webp",
         thumb: "images/thumb/worksThumb6.webp",
         title: "The Wheel Of My Life",
         lang: "en",
-        sentence: "Hello everyone, I’m Jumpei Suko and I’d like to present to you about my Wheel of Life. \n\
-        First, I’d like to talk about my hub. My hub represents Japanese festivals as a metaphor for my emotions and personality. I believe that human beings are strongly influenced by the scenery they have seen and that the scenery evokes feelings they have experienced in the past. I titled my work '情景反射','scenic reflection' by combining the word '条件反射', 'conditional reflection' with the meaning of the word. I also chose Japanese festivals as a motif because I thought they are special but appropriate for everyday scenery. In addition, there is a back and front side to my hub because I believe there is no such thing as a person without many sides. The red is an extroverted side while the blue is an introverted side. In both cases, a candy apple is placed in the center. This is because the apple is a fruit associated with beginnings and because events often occur in my life that are triggered by apples.\n\
+        sentence: "This work was created in a class called art&religion. The assignment was to analyze, create, and present my wheel of life. The presentation is part of the work, so I have included the draft.\n\n\
+        Hello everyone, I’m Jumpei Suko and I’d like to present to you about my Wheel of Life. \n\n\
+        First, I’d like to talk about my hub. My hub represents Japanese festivals as a metaphor for my emotions and personality. I believe that human beings are strongly influenced by the scenery they have seen and that the scenery evokes feelings they have experienced in the past. I titled my work '情景反射','scenic reflection' by combining the word '条件反射', 'conditional reflection' with the meaning of the word. I also chose Japanese festivals as a motif because I thought they are special but appropriate for everyday scenery. In addition, there is a back and front side to my hub because I believe there is no such thing as a person without many sides. The red is an extroverted side while the blue is an introverted side. In both cases, a candy apple is placed in the center. This is because the apple is a fruit associated with beginnings and because events often occur in my life that are triggered by apples.\n\n\
         Next, I’d like to talk about my six realms. \n\
         The first realm represents interest and the motif is a soda called “ramune” in Japan. I am enthusiastic when I am interested in something new, but I lose interest quickly, which is represented as carbonation.\n\
         The second realm represents vanity and the motif is the goldfish. It represents me maintaining a façade in front of others but my self-esteem is low. Goldfish are creatures created by human beings through breeding for ornamental purposes. I chose the goldfish as a motif because I thought it resembled the way humans try to make themselves look good.\n\
         The third realm represents spaciousness, and the motif is the blank space. It shows a big difference inside when there is room as opposed to when there isn’t any. It also represents to others that I appear to be a person who has blank space.\n\
         The fourth realm represents delicacy, and its motif is fireworks. I have a very sensitive side and a rough side, and I am sensitive to relationships.\n\
         The fifth realm represents health and the motif is the yukata pattern. I believe that to live a healthy life means to live a life that takes care of oneself. Wearing a yukata is healthy in that sense, so I chose yukata as the motif. It represents being passionate towards others but cold towards oneself.\n\
-        The sixth realm/last realm represents instability and the motif is a water balloon. I believe that human beings are unstable creatures. Both externally and inwardly, it appears unstable, but it represents fluidity.\n\
+        The sixth realm/last realm represents instability and the motif is a water balloon. I believe that human beings are unstable creatures. Both externally and inwardly, it appears unstable, but it represents fluidity.\n\n\
         Finally, the rim represents the persona and the emotion.\n\
-        The masks represent persona and the circles represent emotions. It also represents the fact that I have little understanding of my emotions. Just as everyone wears a mask in front of others because life is hard, I live my life wearing different masks such as a teacher, a student, and a friend.\n\
-        In conclusion, my hub represents my emotions and personality. A human is very complex, so I don’t represent just one side. What I've described so far is not all of my hub, not all of my inside, and what I perceive is not all of me. If this presentation made you feel like a completely different person, that would also be true. Please share your thoughts after listening to this presentation.\n\
+        The masks represent persona and the circles represent emotions. It also represents the fact that I have little understanding of my emotions. Just as everyone wears a mask in front of others because life is hard, I live my life wearing different masks such as a teacher, a student, and a friend.\n\n\
+        In conclusion, my hub represents my emotions and personality. A human is very complex, so I don’t represent just one side. What I've described so far is not all of my hub, not all of my inside, and what I perceive is not all of me. If this presentation made you feel like a completely different person, that would also be true. Please share your thoughts after listening to this presentation.\n\n\
         Thank you for listening to my presentation. "
     },
     {
-        id :"6",
+        id: "3",
         category: "web",
         content: "images/thumb/worksThumb7.webp",
         thumb: "images/thumb/worksThumb7.webp",
@@ -91,7 +64,73 @@ const Works = [
         In the design, talent was used as the motif, and a collage using each piece of artwork was used as the main visual. The design also evokes the future and creativity, as is typical of our university.",
         lang: "en",
         url: "https://sa-exhibition.com/"
+    },
+    {
+        id: "4",
+        category: "web",
+        content: "images/image/worksImage10.webp",
+        thumb: "images/thumb/worksThumb10.webp",
+        title: "OteteSakka.",
+        sentence: "Otetesakka is a creative experience event for children created by five people including myself. I am still a core member there and continue to work every day to give children as good an experience as possible.\n\n\
+        I started the project 'Otekasakka' after participating in a business contest called 'Future Accelerator Gateway' held by Marui Group Inc. and my internship company, Life-Is-Tech Inc. I was mainly in charge of concept design. It is to create the plan, concept, and worldview of Otesakka. I was also in charge of design and website production.\n\n\
+        If you want to know more about Otetesakka, please visit our website.\n\n\n\n\
+        おててさっかとは、私を含め5人で制作した、子供に向けた創作体験イベントです。今も私はおててさっかの基幹メンバーとして、子供に少しでも良い体験を与えられるように日々活動を続けています。\n\n\
+        最初は株式会社丸井グループ、また自分のインターン先であるライフイズテック株式会社が開催する『Future Accelerator Gateway』というビジネスコンテストに参加したことをきっかけに、プロジェクト「おててさっか」を立ち上げました。私は主に、コンセプトデザインを担当していました。おててさっかの企画やコンセプト、世界観を作ることです。また、デザインやWebサイト制作も担当していました。\n\n\
+        おててさっかの概要をもっと知りたい方は、webサイトをご覧ください。\n\n\
+        https://otetesakka.com/",
+        lang: "en",
+        url: "https://otetesakka.com/"
+    },
+    {
+        id: "5",
+        category: "design",
+        content: "images/image/worksImage2.webp",
+        thumb: "images/thumb/worksThumb2.webp",
+        title: "night in the train.",
+        lang: "en",
+        sentence: "This is my first design piece and is my business card as a programming educator. The motif is a ticket, and the purpose of this work was to communicate what I value and to get people interested in learning more about me."
+    },
+    {
+        id: "6",
+        category: "design",
+        content: "images/image/worksImage3.webp",
+        thumb: "images/thumb/worksThumb3.webp",
+        title: "future.",
+        lang: "en",
+        sentence: "I don't think about my future deeply. "
+    },
+    {
+        id: "7",
+        category: "story",
+        content: "images/image/worksImage9.webp",
+        thumb: "images/thumb/worksThumb9.webp",
+        title: "Bibli.",
+        sentence: "「ここはビブリ。この国にはなんでもある。星が沈むみずうみも、とけないこおりでできたおはなも、ほうせきがふるよるもある。でもたった1つ、ないものがある。それは、色。色だけがなくなってしまう国。」\n\n\
+        子供向け創作体験イベントおててさっかにて、お話の作成とコンセプトアートを作成。色がなくなってしまう国を、純粋な心で色を描いていくお話。",
+        lang: "ja",
     }
+    ,
+    {
+        id: "8",
+        category: "design",
+        content: "images/image/worksImage4.webp",
+        thumb: "images/thumb/worksThumb4.webp",
+        title: "witch",
+        lang: "en",
+        sentence: "illustration"
+    },
+    {
+        id: "9",
+        category: "web",
+        content: "images/thumb/worksThumb8.webp",
+        thumb: "images/thumb/worksThumb8.webp",
+        title: "hibi-to-mabuka.",
+        sentence: "Created a website to introduce Manuka honey as a web design study.\n\
+        The design was based on shapes in order to break away from the reliance on photographs.",
+        lang: "en",
+        url: "https://hibi-to-manuka.netlify.app/"
+    },
+    
 ];
 
 export default Works
